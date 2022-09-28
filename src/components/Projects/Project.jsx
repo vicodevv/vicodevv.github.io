74109a1675c36fd8254d28d55e730bdaca4ee3c2
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-29 00:28:14 +0100

    added projects icons

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -7,28 +7,33 @@ const Project = () =>{
             <div className="project-container">
                 <div className="project">
                     <a href="https://github.com/vicodevv/Library-API">Library API</a>
-                    <p></p>
                     <p className='info'>This is a BookstoreAPI which performs the basic CRUD operations</p>
+                    <div className="icons"></div>
                 </div>
                 <div className="project">
                     <a href="https://github.com/vicodevv/Student-API">Student API</a>
                     <p className='info'>This is an API which is used to get students of a University.</p>
+                    <div className="icons"></div>
                 </div>
                 <div className="project">
                     <a href="https://github.com/vicodevv/Ticket-API">Ticket API</a>
                     <p className='info'>This is an API which is used to create tickets and events.</p>
+                    <div className="icons"></div>
                 </div>
                 <div className="project">
                     <a href="https://github.com/vicodevv/twitter-trends">Twitter Trends</a>
                     <p className='info'>This is used to get trending topics from a particular location on twitter.</p>
+                    <div className="icons"></div>
                 </div>
                 <div className="project">
                     <a href="https://github.com/vicodevv/netflix-clone">Netflix clone</a>
                     <p className='info'>Mobile responsive Netflix clone built with ReactJS, Typescript, styled - component, react router, Firebase and context API for state management.</p>
+                    <div className="icons"></div>
                 </div>
                 <div className="project">
                     <a href="https://github.com/vicodevv/weather-app">Weather app</a>
                     <p className='info'>A weather app</p>
+                    <div className="icons"></div>
                 </div>
             </div>
         </div>
