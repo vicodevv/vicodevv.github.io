471bb3365555b4797b68f6ed4cb1a3b89c54bcee
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-28 18:30:01 +0100

    more tools images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Tools from './components/Tools/Tools';
 function App() {
   return (
    <Route exact path="/">
       <Home />
+      <Tools />
     </Route>
   );
 }
--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -1,29 +1,56 @@
 import react from '../../assets/react.png';
-import redux from '../../assets/redux.png';
 import node from '../../assets/node.png';
-import express from '../../assets/express.png';
 import mongodb from '../../assets/mongodb.png';
-import postgresql from '../../assets/postgresql.png';
-import golang from '../../assets/golang.png';
+import express from '../../assets/express.png';
+import nest from '../../assets/nest.png';
+import django from '../../assets/django.png';
 import springboot from '../../assets/springboot.png';
-import docker from '../../assets/docker.png';
-import kubernetes from '../../assets/kubernetes.png';
+import javascript from '../../assets/javascript.png';
+import sass from '../../assets/sass.png';
+import python from '../../assets/python.png';
+import typescript from '../../assets/typescript.png';
+import golang from '../../assets/golang.png';
+import java from '../../assets/java.png';
+import aws from '../../assets/aws.png';
+import docker from '../../assets/docker.jpeg';
+import postman from '../../assets/postman.png';
+import github from '../../assets/github.png';
+
+
+
+import './Tools.scss';
 
 const Tools = () => {
-    return(
-        div id="tools">
-            <><p className='header'>Tools & Technologies</p><div className="images">
-                <img src={react} alt="react" />
-                <img src={redux} alt="redux" />
-                <img src={node} alt="node" />
-                <img src={express} alt="express" />
-                <img src={mongodb} alt="mongodb" />
-                <img src={postgresql} alt="postgresql" />
-                <img src={golang} alt="golang" />
-                <img src={springboot} alt="springboot" />
-                <img src={docker} alt="docker" />
-                <img src={kubernetes} alt="kubernetes" />
-            </div></>
-        </div>
-    )
-}
+    const images = [
+        react,
+        node,
+        mongodb,
+        express,
+        nest,
+        django,
+        springboot,
+        javascript,
+        sass,
+        python,
+        typescript,
+        golang,
+        java,
+        aws,
+        docker,
+        postman,
+        github,
+    ];
+
+return(
+    <div id="tools">
+        <p className='header'>Tools & Technologies</p>
+            <div className="images">
+                {images.map((image, index) => (
+                    <img src={image} alt={index} />
+                ))}
+            </div>
+    </div>
+    );
+};
+
+export default Tools;
