250f8921c39de9f379b61f451f515dfb40d4244e
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-28 17:19:09 +0100

    intro-section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import './Home.scss';
+import { SiTwitter, SiGithub, SiLinkedin } from 'react-icons/si';
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1>Home</h1>
+    <div id="home">
+        <div className="container">
+            <div className="bio">
+                <div className="intro">Hey there, my name is</div>
+                <div className="name">Omoniyi Victor</div>
+                <div className="social">
+                    <a href="https://github.com/vicodevv">
+                        <SiGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/victor-omoniyi-56a65218b/">
+                        <SiLinkedin />
+                    </a>
+                    <a href="https://twitter.com/vicodev_">
+                        <SiTwitter />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
